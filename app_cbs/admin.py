63b3309bf41045fe6ccaaac64ec74972cfa14f3f
NicholasBallard <nicholasericballard@gmail.com>
2017-10-13 22:16:38 -0500

    Adding models.py initial db migration and adding test items to Thing

--- app_cbs/admin.py
+++ app_cbs/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# import your model
+from app_cbs.models import Thing
+
+# set up automated slug creation
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it
+admin.site.register(Thing, ThingAdmin)
