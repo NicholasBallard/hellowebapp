9839c4d0d20cd62ea562e69b4a8e0e37a36c67ed
NicholasBallard <nicholasericballard@gmail.com>
2017-10-15 23:35:19 -0500

    Make a page for each item in the database

--- app_cbs/views.py
+++ app_cbs/views.py
@@ -4,9 +4,18 @@ from app_cbs.models import Thing
 # Create your views here.
 def index(request):
     # defining the variable
-    things = Thing.objects.all().order_by('?')
+    things = Thing.objects.all()
     # passing the variable to the view
     return render(request, 'index.html', {
         # don't forget to pass it in, and the last comma
         'things': things,
+    })
+
+def thing_detail(request, slug):
+    # grab the object...
+    thing = Thing.objects.get(slug=slug)
+
+    # and pass to the template
+    return render(request, 'things/thing_detail.html', {
+        'thing': thing,
     })
