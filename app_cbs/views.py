b2599d5b1781c6427f99bc0d9c68f9922f39328c
NicholasBallard <nicholasericballard@gmail.com>
2017-10-15 21:22:38 -0500

    Changed to query all in database in random order

--- app_cbs/views.py
+++ app_cbs/views.py
@@ -4,7 +4,7 @@ from app_cbs.models import Thing
 # Create your views here.
 def index(request):
     # defining the variable
-    things = Thing.objects.filter(name__contains='Samsung')
+    things = Thing.objects.all().order_by('?')
     # passing the variable to the view
     return render(request, 'index.html', {
         # don't forget to pass it in, and the last comma
