2f6e9daac8392f3a5f9a3b3f4513a0f3b4e800f6
NicholasBallard <nicholasericballard@gmail.com>
2017-10-17 15:26:22 -0500

    Added forms to edit dynamically created pages.

--- app_cbs/views.py
+++ app_cbs/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app_cbs.forms import ThingForm
 from app_cbs.models import Thing
 
 # Create your views here.
@@ -18,4 +19,29 @@ def thing_detail(request, slug):
     # and pass to the template
     return render(request, 'things/thing_detail.html', {
         'thing': thing,
+    })
+
+def edit_thing(request, slug):
+    # grab the object
+    thing = Thing.objects.get(slug=slug)
+    # set the form we're using
+    form_class = ThingForm
+
+    # if we're coming to this view from a submitted form
+    if request.method == 'POST':
+        # grab the data from the submitted form and apply
+        # it to the form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
     })
